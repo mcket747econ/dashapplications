c738ed01dc945be94ab21529a3635143e0205d18
mcket747econ <59522800+mcket747econ@users.noreply.github.com>
2020-07-01 12:20:27 -0400

    Initial Launch

--- myattempt.py
+++ myattempt.py
@@ -13,25 +13,26 @@ import dash
 app = dash.Dash('Student Loans Data')
 server = app.server
 
-external_css = ["https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"]
-external_js = ['https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js']
+# external_css = ["https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"]
+# external_js = ['https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js']
 
-app = dash.Dash('loansdata',
-                external_scripts=external_js,
-                external_stylesheets=external_css)
+# app = dash.Dash('loansdata',
+#                 external_scripts=external_js,
+#                 external_stylesheets=external_css)
 
-df = pd.read_excel(r'../dashapp/loans_sum.xlsx')
+df = pd.read_excel(r'../dashapplications/student_loans.xls')
+print(df)
 app.layout = html.Div([
     html.H1('Student Loans Data'),
     dcc.Graph(id='loans',
               figure ={
-                  'data':[{'x': df['observation_date'],'y':df['SLOAS(Billions)'],'type':'line','name':'boats'}],
+                  'data':[{'x': df['observation_date'],'y':df['SLOAS'],'type':'line','name':'boats'}],
                   'layout':{
                       'title':'Total Student Loans Trend'
                       
                   }
               })
 ])
-erver.secret_key = os.environ.get(‘SECRET_KEY’, ‘my-secret-key’)
+
 if __name__ =='__main__':
     app.run_server(debug=True)
